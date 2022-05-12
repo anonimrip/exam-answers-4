18f812766a3df206f000c2e1b15f399f6bbba246
EREN FELEKOGLU <EREN FELEKOGLU@BIL-12>
2022-05-12 11:29:03 +0300

    Proje dosyası ekle.

---
+++ Soru4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//ER3N
+namespace Soru4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            
+            
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            int sayac = 10;
+            for (int c = 1; c <= 100; c++)
+            {
+                if (c == sayac)
+                {
+                    sayac = sayac+10;
+                    continue;
+                }
+                Console.WriteLine(c);
+            }
+
+        }
+    }
+}
